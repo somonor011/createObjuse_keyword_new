4c0e2183097e59e6cf78d1d21dd3e44f31bd9886
Somonor Hong <150505791+somonor011@users.noreply.github.com>
2024-03-06 20:01:56 +0700

    Add files via upload

---
+++ create_obj_use_newkeyword/app.js
@@ -0,0 +1,10 @@
+// create object use keyword < new >
+
+const obj = new Object(); // new is keyword
+obj.name = "Somonor";
+obj.age = 18;
+obj.coding = function coding(){
+    return "Yeah, I can write code ..."
+}
+
+console.log(obj.name);
